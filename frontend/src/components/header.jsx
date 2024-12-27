77e376aa8cef72a83468b3714be127521dff32a6
Madhan-767 <madhanmohanreddy767@gmail.com>
2024-12-27 16:21:11 +0530

    added styling to the header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,8 +1,13 @@
+import LoginPage from "./loginPage";
+import '../css/header.css'
 const Header = () => {
   return(
+    <>
     <div className="header">
       <h1>Online banking system</h1>
     </div>
+      <LoginPage />
+      </>
   )
 }
 
