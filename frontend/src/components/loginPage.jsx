4aeab2d2957c4d26d092128157870117e1387e71
Madhan-767 <madhanmohanreddy767@gmail.com>
2024-12-27 16:28:57 +0530

    added some styles to login form

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -1,11 +1,16 @@
+import '../css/login.css'
+
 const LoginPage = () =>{
   return(
     <div className="loginForm">
-      <label htmlFor='username'>Username: </label>
-     <input type='text' id='username' placeholder='Enter your username' ></input>
-
+      <div className='userNameContainer' >
+        <label htmlFor='username'>Username: </label>
+        <input type='text' id='username' placeholder='Enter your username' ></input>
+      </div>
+   <div className='passwordContainer' >
      <label htmlFor='password'>Password</label>
      <input type='password' placeholder='Enter your password' id='password'></input>
+   </div>
     </div>
   )
 };
