2d23afabdc12e3fbaf0c01ccc65e924071d93183
Madhan-767 <madhanmohanreddy767@gmail.com>
2024-12-28 09:45:38 +0530

    completed css for login page

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -2,16 +2,19 @@ import '../css/login.css'
 
 const LoginPage = () =>{
   return(
-    <div className="loginForm">
+   <div className='loginFormMainContainer' >
+     <div className="loginForm">
       <div className='userNameContainer' >
         <label htmlFor='username'>Username: </label>
         <input type='text' id='username' placeholder='Enter your username' ></input>
       </div>
    <div className='passwordContainer' >
-     <label htmlFor='password'>Password</label>
+     <label htmlFor='password'>Password: </label>
      <input type='password' placeholder='Enter your password' id='password'></input>
    </div>
+   <button className='loginButton' >Login</button>
     </div>
+   </div>
   )
 };
 export default LoginPage;
