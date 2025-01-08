519ae695d123c8d26fad3d5e63f793e1cb992e26
JAGADEESH <22022cm040@gmail.com>
2025-01-08 07:31:02 +0530

    made some bakend structure like separate files for differnt purposes

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,18 @@
+const db = require('../models/db');
+
+
+const login = ('/api/admin/login', (req, res) => {
+  const { email, password } = req.body;
+  const query = 'SELECT * FROM admins WHERE email = ? AND password = ?';
+  db.query(query, [email, password], (err, result) => {
+    if (err) {
+      console.error('Database query error: ', err);
+      return res.status(500).json({ success: false, message: 'Database query error' });
+    }
+
+    if (result && result.length > 0) return res.json({ success: true, admin: result[0] });
+    res.status(401).json({ success: false, message: 'Invalid login details' });
+  });
+});
+
+module.exports = { login };
--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,13 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
-
-db.connect(() => {
-  console.log('Connected to the MySQL database');
-});
-
-app.post('/api/admin/login', (req, res) => {
-  const { email, password } = req.body;
-  const query = 'SELECT * FROM admins WHERE email = ? AND password = ?';
-  db.query(query, [email, password], (err, result) => {
-    if (err) {
-      console.error('Database query error: ', err);
-      return res.status(500).json({ success: false, message: 'Database query error' });
-    }
-
-    if (result && result.length > 0) return res.json({ success: true, admin: result[0] });
-    res.status(401).json({ success: false, message: 'Invalid login details' });
-  });
-});
+app.use('/api/admin', adminRoutes);
 
 app.listen(8000, () => {
   console.log('Server running at http://localhost:8000');
