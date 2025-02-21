6ce747401a9b230626088cc5ed581b27813a4ba2
JAGADEESH <22022cm040@gmail.com>
2025-02-22 00:46:22 +0530

    made some changes

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -4,15 +4,17 @@ import "../css/App.css";
 
 const Welcome = () => {
   return (
-    <div className="welcomeContainer">
-      <h1>Welcome to the Magadha Banking</h1>
-      <div className="options">
-        <p className="admins"> <Link to="/adminLogin" className="link">Employee</Link> </p>
-        <p className="user"> <Link to="/login" className="link">Customer</Link></p>
+    <div className="welcome-container">
+      <div className="welcome-content">
+        <h1 className="welcome-heading">Welcome to</h1>
+        <h1 className="welcome-heading">Magadha Banking</h1>
+        <p className="welcome-text">
+          Simple, and Smart Banking. Manage your finances with ease.
+        </p>
+        <Link to="/login" className="welcome-button">Get Started</Link>
       </div>
-
     </div>
   );
-}
+};
 
 export default Welcome;
