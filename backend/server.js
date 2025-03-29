447c34c1ef3fdc14f7177cab89f5dcdbdac72232
JAGADEESH <22022cm040@gmail.com>
2025-03-29 12:47:34 +0530

    fix the issue of while refrehing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
 
 // const authRoutes = require('./routes/authRoutes');
 // const adminRoutes = require('./routes/adminRoutes');
